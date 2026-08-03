0c235a0a259c5cce9211f14fb948467c46668aaf
Jonathan Ng <jonathanng978@gmail.com>
2026-08-03 17:10:25 +0800

    reformatted code

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,20 +1,34 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('test', async ({ page }) => {
-  await page.goto('/index.html');
-  await expect(page.locator('#logo')).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'HP DesignJet Smart Tank T908' })).toBeVisible();
-  await expect(page.getByRole('button', { name: 'Notifications' })).toBeVisible();
-  await expect(page.getByRole('button', { name: 'Search' })).toBeVisible();
-  await page.getByRole('button', { name: 'Sign In/Sign Out' }).click();
-  await page.getByRole('textbox', { name: 'Enter PIN' }).fill(process.env.PRINTER_PIN);
-  await page.getByRole('textbox', { name: 'Enter PIN' }).press('Enter');
-  await expect(page.getByRole('button', { name: 'Sign In/Sign Out' })).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'Tanks' })).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'Paper' })).toBeVisible();
-  await expect(page.getByText('Estimated tank levels. View')).toBeVisible();
-  await expect(page.locator('.mat-grid-tile-content').first()).toBeVisible();
-  await expect(page.locator('mat-grid-tile:nth-child(2) > .mat-grid-tile-content').first()).toBeVisible();
-  await expect(page.locator('mat-grid-tile:nth-child(3) > .mat-grid-tile-content')).toBeVisible();
-  await expect(page.locator('mat-grid-tile:nth-child(4) > .mat-grid-tile-content')).toBeVisible();
+test("test", async ({ page }) => {
+  await page.goto("/index.html");
+  await expect(page.locator("#logo")).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "HP DesignJet Smart Tank T908" }),
+  ).toBeVisible();
+  await expect(
+    page.getByRole("button", { name: "Notifications" }),
+  ).toBeVisible();
+  await expect(page.getByRole("button", { name: "Search" })).toBeVisible();
+  await page.getByRole("button", { name: "Sign In/Sign Out" }).click();
+  await page
+    .getByRole("textbox", { name: "Enter PIN" })
+    .fill(process.env.PRINTER_PIN);
+  await page.getByRole("textbox", { name: "Enter PIN" }).press("Enter");
+  await expect(
+    page.getByRole("button", { name: "Sign In/Sign Out" }),
+  ).toBeVisible();
+  await expect(page.getByRole("heading", { name: "Tanks" })).toBeVisible();
+  await expect(page.getByRole("heading", { name: "Paper" })).toBeVisible();
+  await expect(page.getByText("Estimated tank levels. View")).toBeVisible();
+  await expect(page.locator(".mat-grid-tile-content").first()).toBeVisible();
+  await expect(
+    page.locator("mat-grid-tile:nth-child(2) > .mat-grid-tile-content").first(),
+  ).toBeVisible();
+  await expect(
+    page.locator("mat-grid-tile:nth-child(3) > .mat-grid-tile-content"),
+  ).toBeVisible();
+  await expect(
+    page.locator("mat-grid-tile:nth-child(4) > .mat-grid-tile-content"),
+  ).toBeVisible();
 });
