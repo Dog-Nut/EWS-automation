970ce64e24699f168fb651cd9724e8ef18d8dcb9
Jonathan Ng <jonathanng978@gmail.com>
2026-08-13 16:53:51 +0800

    fixtures for job page

--- pages/jobs-page.js
+++ pages/jobs-page.js
@@ -2,8 +2,17 @@ import { expect, test } from "@playwright/test";
 
 export class JobsPage {
   constructor(page) {
+    this.page = page;
     this.buttons = page.getByRole("button");
     this.tableCells = page.getByRole("cell");
+  }
+
+  async goToJobSettings() {
+    await this.page.getByRole("menuitem", { name: "Jobs" }).click();
+    await this.page
+      .locator("#menu-copy-defaultJobOptions")
+      .getByRole("navigation", { name: "Job Queue" })
+      .click();
   }
 
   async expectButtonVisible(text) {
