32ce4a3b5b3027b25b3bdc1e34783e1be313cdfc
Jonathan Ng <jonathanng978@gmail.com>
2026-08-13 16:53:58 +0800

    fix for copy page

--- tests/copy.spec.js
+++ tests/copy.spec.js
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from "../fixtures";
 import { SignInProcess } from "../pages/signIn-process";
 import { CopySettingsPage } from "../pages/copy-settings-page";
 
